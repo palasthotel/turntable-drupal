d056e973742886d6201f89160afd617739a808ff
Paul Vorbach <paul@vorba.ch>
2014-05-27 15:37:44 +0200

    Update client forms

--- turntable_client/turntable_client.node.php
+++ turntable_client/turntable_client.node.php
@@ -1,10 +1,42 @@
 <?php
-$form['turntable_client_master_url'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Turntable Master URL'),
-    '#default_value' => variable_get('turntable_client_master_url', array(
-        'http://turntable-master.palasthotel.de/'
-    )),
-    '#description' => t('The URL of the linked Turntable Master instance.')
-);
-$form['#submit'][] = 'turntable_client_admin_settings_submit';
+
+function turntable_client_sync_node_settings() {
+  $form['turntable_client_sync_push'] = array(
+    '#title' => t('Push'),
+    '#type' => 'fieldset',
+    '#description' => t('Push local node to the Turntable Master.')
+  );
+
+  $form['turntable_client_sync_push']['submit'] = array(
+    '#type' => 'button',
+    '#value' => t('Push to master')
+  );
+
+  $form['turntable_client_sync_pull'] = array(
+    '#title' => t('Pull'),
+    '#type' => 'fieldset',
+    '#description' => t('Pull remote node from the Turntable Master.')
+  );
+
+  $form['turntable_client_sync_pull']['sync_type'] = array(
+    '#type' => 'value',
+    '#value' => array(
+      t('Copy'),
+      t('Reference')
+    )
+  );
+
+  $form['turntable_client_sync_pull']['sync_type_select'] = array(
+    '#type' => 'select',
+    '#title' => t('Synchronization Policy'),
+    '#description' => t('Determines if the local node is a copy of the master node or a reference that gets updated on remote changes (local changes will be overwritten).'),
+    '#options' => $form['turntable_client_sync_pull']['sync_type']['#value']
+  );
+
+  $form['turntable_client_sync_pull']['submit'] = array(
+    '#type' => 'button',
+    '#value' => t('Update')
+  );
+
+  return $form;
+}
