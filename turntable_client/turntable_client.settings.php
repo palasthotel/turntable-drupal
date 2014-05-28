0bdedd3060a3e93080d8b0147ff839b40af810e7
Paul Vorbach <paul@vorba.ch>
2014-05-28 11:19:55 +0200

    Correctly use variables

--- turntable_client/turntable_client.settings.php
+++ turntable_client/turntable_client.settings.php
@@ -18,11 +18,15 @@ function turntable_client_admin_settings() {
   $form['turntable_client_master_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Turntable Master URL'),
-    '#default_value' => variable_get('turntable_client_master_url', array(
-      'http://turntable-master.palasthotel.de/'
-    )),
+    '#default_value' => variable_get('turntable_client_master_url',
+        'http://turntable-master.palasthotel.de/'),
     '#description' => t('The URL of the linked Turntable Master instance.')
   );
   $form['#submit'][] = 'turntable_client_admin_settings_submit';
   return system_settings_form($form);
+}
+
+function turntable_client_admin_settings_submit(&$form, &$form_state) {
+  variable_set('turntable_client_master_url',
+      $form_state['values']['turntable_client_master_url']);
 }
