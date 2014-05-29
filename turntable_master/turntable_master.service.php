dfbda82f7bc84e73037efbb68f9d0844a5fdf12f
Paul Vorbach <paul@vorba.ch>
2014-05-29 22:16:04 +0200

    Debuggable requests

--- turntable_master/turntable_master.service.php
+++ turntable_master/turntable_master.service.php
@@ -1,5 +1,15 @@
 <?php
 
 function turntable_master_create_shared_node($data) {
-  debug($data);
+  print_r($data);
+}
+
+function turntable_master_update_shared_node($nid, $data) {
+  print_r($nid);
+
+  print_r($data);
+}
+
+function turntable_master_index_shared_nodes() {
+  return array();
 }
