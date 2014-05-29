6df833ef4271250b026b5870325c42dc55cb650c
Paul Vorbach <paul@vorba.ch>
2014-05-29 10:15:14 +0200

    Adjust according to latest changes in table names etc.

--- turntable_client/turntable_client.node.php
+++ turntable_client/turntable_client.node.php
@@ -1,7 +1,20 @@
 <?php
 require_once './sites/all/libraries/turntable/turntable_client.php';
 
+function get_node_id() {
+  if (!is_numeric(arg(1))) {
+    return -1;
+  }
+
+  // return the node's id
+  return (int) arg(1);
+}
+
 function turntable_client_shared_state_settings() {
+  $nid = get_node_id();
+
+  $shared_state = turntable_client::getInstance()->getDB()->getSharedState($nid);
+
   $form['turntable_client_shared_states'] = array(
     '#type' => 'value',
     '#value' => array(
@@ -17,7 +30,8 @@ function turntable_client_shared_state_settings() {
     '#title' => t('Shared state'),
     '#description' => t(
         'Determines if the local node is not shared, a copy of a node on the master, a reference that gets updated on remote changes (local changes will be overwritten), or the original (remote nodes will get updated).'),
-    '#options' => $form['turntable_client_shared_states']['#value']
+    '#options' => $form['turntable_client_shared_states']['#value'],
+    '#default_value' => $shared_state
   );
 
   $form['submit'] = array(
@@ -37,12 +51,12 @@ function turntable_client_shared_state_settings_submit(&$form, &$form_state) {
   $nid = (int) arg(1);
 
   // form settings
-  $shared_state = $form_state['values']['shared_state'];
+  $shared_state = $form_state['values']['turntable_client_shared_state'];
 
   $client = turntable_client::getInstance();
   $db = $client->getDB();
 
-  $db->setSynchronizationSettings($nid, $shared_state);
+  $db->setSharedState($nid, $shared_state);
 }
 
 function turntable_client_node_presave($node) {
