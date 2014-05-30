c43426d675dc78557c686af4608c74881e735f37
Paul Vorbach <paul@vorba.ch>
2014-05-30 10:03:45 +0200

    Change request handlers

--- turntable_master/turntable_master.service.php
+++ turntable_master/turntable_master.service.php
@@ -1,15 +1,12 @@
 <?php
+require_once './sites/all/libraries/turntable/turntable_master.php';
 
-function turntable_master_create_shared_node($data) {
-  print_r($data);
-}
-
-function turntable_master_update_shared_node($nid, $data) {
-  print_r($nid);
-
+function turntable_master_send_shared_node($data) {
+  // handle request
   print_r($data);
 }
 
 function turntable_master_index_shared_nodes() {
+  // TODO to be implemented
   return array();
 }
