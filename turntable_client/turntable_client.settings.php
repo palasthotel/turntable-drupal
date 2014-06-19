0419694d9a6911424b1277cbf3e5bcead1e7afa5
Paul Vorbach <paul@vorba.ch>
2014-06-19 16:40:40 +0200

    Add configuration var for update interval

--- turntable_client/turntable_client.settings.php
+++ turntable_client/turntable_client.settings.php
@@ -21,11 +21,26 @@ function turntable_client_admin_settings() {
     '#default_value' => variable_get('turntable_client_master_url'),
     '#description' => t('The URL of the linked Turntable Master instance.')
   );
+
+  $form['turntable_client_update_interval'] = array(
+    '#type' => 'select',
+    '#title' => t('Turntable update interval'),
+    '#default_value' => variable_get('turntable_client_update_interval'),
+    '#description' => t(
+        'Number of hours between automatic updates of referenced nodes.')
+  );
+
   $form['#submit'][] = 'turntable_client_admin_settings_submit';
+
   return system_settings_form($form);
 }
 
 function turntable_client_admin_settings_submit(&$form, &$form_state) {
+  // set master url
   variable_set('turntable_client_master_url',
       $form_state['values']['turntable_client_master_url']);
+
+  // set update interval
+  variable_set('turntable_client_update_interval',
+      $form_state['values']['turntable_client_update_interval']);
 }
