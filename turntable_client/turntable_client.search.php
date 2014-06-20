446295da6acf057db39366c4d4e31c203cb5cae8
Paul Vorbach <paul@vorba.ch>
2014-06-20 11:30:20 +0200

    Add dummy search form results

--- turntable_client/turntable_client.search.php
+++ turntable_client/turntable_client.search.php
@@ -1,6 +1,6 @@
 <?php
 
-function turntable_client_content_search() {
+function turntable_client_content_search($form, &$form_state) {
   $form['turntable_client_content_search'] = array(
     '#type' => 'textfield',
     '#title' => t('Query'),
@@ -8,11 +8,34 @@ function turntable_client_content_search() {
         'Search terms. Searches all fields that are shared within the node.')
   );
   $form['#submit'][] = 'turntable_client_content_search_submit';
-  $form['submit'] = array('#type' => 'submit', '#value' => t('Search'));
+  $form['submit'] = array(
+    '#type' => 'submit',
+    '#value' => t('Search')
+  );
+
+  if (!empty($form_state['values']['turntable_client_content_search'])) {
+    debug($form_state['values']['turntable_client_content_search']);
+    $form['results'] = array(
+      '#type' => 'table',
+      '#title' => t('Results'),
+      '#tree' => TRUE,
+      '#theme' => 'table',
+      '#header' => array(
+        t('Title'),
+        t('Date')
+      ),
+      '#rows' => array(
+        array(
+          'a',
+          'b'
+        )
+      )
+    );
+  }
 
   return $form;
 }
 
-function turntable_client_content_search_submit(&$form, &$form_state) {
-  debug($form_state);
+function turntable_client_content_search_submit($form, &$form_state) {
+  $form_state['rebuild'] = TRUE;
 }
