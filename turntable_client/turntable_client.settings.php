660063cbade31006b204806764e0e61ca5feb844
Paul Vorbach <paul@vorba.ch>
2014-10-09 16:05:06 +0200

    Default node type settings

--- turntable_client/turntable_client.settings.php
+++ turntable_client/turntable_client.settings.php
@@ -39,9 +39,40 @@ function turntable_client_admin_settings() {
     '#description' => t('Unique string that is used to identify a Client.')
   );
 
+  $node_types = get_node_types();
+  $node_types_selection = variable_get('turntable_client_share_node_types',
+      array(
+        'article',
+        'page'
+      ));
+
+  $form['turntable_client_share_node_types'] = array(
+    '#type' => 'checkboxes',
+    '#title' => t('Shared node types'),
+    '#options' => $node_types,
+    '#default_value' => $node_types_selection,
+    '#description' => t(
+        'Node types that are shared by default when they are saved.')
+  );
+
   $form['#submit'][] = 'turntable_client_admin_settings_submit';
 
   return system_settings_form($form);
+}
+
+/**
+ * Returns a list of node types.
+ *
+ * @return array
+ */
+function get_node_types() {
+  $node_types = array();
+
+  foreach (node_type_get_types() as $type => $obj) {
+    $node_types[$type] = $obj->name;
+  }
+
+  return $node_types;
 }
 
 function turntable_client_admin_settings_submit(&$form, &$form_state) {
@@ -56,4 +87,16 @@ function turntable_client_admin_settings_submit(&$form, &$form_state) {
   // set client id
   variable_set('turntable_client_id',
       $form_state['values']['turntable_client_id']);
+
+  // get the string we need to store
+  $selection = $form_state['values']['turntable_client_share_node_types'];
+  $selected_node_types = array();
+  foreach ($selection as $key => $value) {
+    if ($value !== 0) {
+      $selected_node_types[] = $value;
+    }
+  }
+
+  // set selected node types
+  variable_set('turntable_client_share_node_types', $selected_node_types);
 }
