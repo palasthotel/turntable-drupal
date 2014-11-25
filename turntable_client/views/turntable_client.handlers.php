9e30da3cc87af4a481a6552261a29d0500fdd223
Paul Vorbach <paul@vorba.ch>
2014-11-25 16:06:33 +0100

    Fix date handler

--- turntable_client/views/turntable_client.handlers.php
+++ turntable_client/views/turntable_client.handlers.php
@@ -36,20 +36,5 @@ class turntable_shared_state_date_handler extends views_handler_field_date {
   public function render($values) {
     $value = $this->get_value($values);
     return $value;
-    switch ($value) {
-      case '0' :
-        return t('Not shared');
-        break;
-      case '1' :
-        return t('Copy');
-        break;
-      case '2' :
-        return t('Reference');
-        break;
-      case '3' :
-        return t('Original node');
-      default :
-        return '';
-    }
   }
 }
