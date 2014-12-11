50a293cf3e86e370fdfdef5e60d584443ab5e14c
Paul Vorbach <paul@vorba.ch>
2014-12-11 11:01:15 +0100

    Remove (unused) update interval settings

--- turntable_client/turntable_client.settings.php
+++ turntable_client/turntable_client.settings.php
@@ -24,14 +24,6 @@ function turntable_client_admin_settings() {
     '#description' => t('The URL of the linked Turntable Master instance.')
   );
 
-  $form['turntable_client_update_interval'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Turntable update interval'),
-    '#default_value' => variable_get('turntable_client_update_interval'),
-    '#description' => t(
-        'Number of hours between automatic updates of referenced nodes.')
-  );
-
   $form['turntable_client_upload_limit'] = array(
     '#type' => 'textfield',
     '#title' => t('Turntable upload limit'),
@@ -94,10 +86,6 @@ function turntable_client_admin_settings_submit(&$form, &$form_state) {
   // set master url
   variable_set('turntable_client_master_url',
       $form_state['values']['turntable_client_master_url']);
-
-  // set update interval
-  variable_set('turntable_client_update_interval',
-      (int) $form_state['values']['turntable_client_update_interval']);
 
   // set upload limit
   variable_set('turntable_client_upload_limit',
